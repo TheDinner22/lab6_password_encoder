c511fbd39c0effa0c42d6e89c6fb4cebdbffe9bf
joe goodman <jgood1133@gmail.com>
2023-02-28 14:47:26 -0500

    encoder impl

--- encoder.py
+++ encoder.py
@@ -58,6 +58,42 @@ def main():
             # the input were not 1, 2, or 3
             error("unreachable!")
 
+# encode a password by shifting each number up by three.
+# 9 wraps back around to 0
+#
+# password is a string containing ONLY numbers. Ex: "123123" or "57857439"
+# This function returns a string.
+#
+# this function assumes the input is valid and will crash if it's not
+def encode_password(password):
+    encoded_password = ""
+
+    # loop over every character
+    for char in password:
+        # convert char to number
+        char = int(char)
+
+        # if the char is 7 or greater, adding 3 to it would cause an overflow,
+        # (i.e 8's encoding is not 8 + 3)
+        # so I hard code the encodings for 7, 8, and 9
+
+        if char == 9:
+            encoded_password += "2"
+
+        elif char == 8:
+            encoded_password += "1"
+
+        elif char == 7:
+            encoded_password += "0"
+
+        # in every other case, I can just add 3 to
+        # the char to get its encoding
+        # (i.e 3's encoding is 3 + 3)
+
+        else:
+            encoded_password += str(char + 3)
+
+    return encoded_password
 
 if __name__ == "__main__":
     main()
