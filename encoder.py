a356fb89287eb29d1e018ab990709511cab2b25e
joe goodman <jgood1133@gmail.com>
2023-02-28 14:22:35 -0500

    added print_menu function

--- encoder.py
+++ encoder.py
@@ -2,6 +2,15 @@
 # 02/28/23
 # encode passwords and then decode them
 
+def print_menu():
+    print("")
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print("")
+
 def main():
     pass
 
