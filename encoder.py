8f4b24210d7a9eff4145b903a2c9c283334f6d50
joe goodman <jgood1133@gmail.com>
2023-02-28 14:29:50 -0500

    error function and checked input function

--- encoder.py
+++ encoder.py
@@ -2,6 +2,13 @@
 # 02/28/23
 # encode passwords and then decode them
 
+# crash the program with a custom error message
+# msg is a string
+# this function returns nothing and will crash the program
+def error(msg):
+    raise Exception(msg)
+
+# prints the menu plus whitespace, nothing returned
 def print_menu():
     print("")
     print("Menu")
@@ -10,6 +17,21 @@ def print_menu():
     print("2. Decode")
     print("3. Quit")
     print("")
+
+# get input after displaying the menu to the user
+# the input should only ever be "1", "2", or "3"
+#
+# this function returns the input as a number 
+# UNLESS
+# the input is invalid, in which case the program crashes
+def menu_input():
+    user_input = int(input("Please enter an option: "))
+
+    # if input is invalid, crash
+    if user_input < 1 or user_input > 3:
+        error("invalid input!!!")
+
+    return user_input
 
 def main():
     pass
